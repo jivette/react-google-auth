30a48249dd26557fda0ac76754919761c46b5592
Ivette <ijuarez@masterdevel.com>
2019-11-26 13:32:07 -0600

    add basic project structure

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyC-rg3Lo0AKu_dg6vik7WjrvXD-6tM2mYo",
+    authDomain: "auth-24506.firebaseapp.com",
+    databaseURL: "https://auth-24506.firebaseio.com",
+    projectId: "auth-24506",
+    storageBucket: "auth-24506.appspot.com",
+    messagingSenderId: "887947117471",
+    appId: "1:887947117471:web:84a88fb06285b5afbfb3aa",
+    measurementId: "G-6Q23FKMDL7"
+}
